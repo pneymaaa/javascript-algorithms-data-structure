27a6c72b31c76a7791400d88890732262bdde10f
pneymaa <pneymila@gmail.com>
2022-01-13 21:40:55 +0700

    Fixing TelephoneNoValidator

--- TelephoneNoValidator/index.js
+++ TelephoneNoValidator/index.js
@@ -1,9 +1,14 @@
 function telephoneCheck(str) {
-    // let a = /\(/g.test(str);
-    // let b = /\)/g.test(str);
-    // let newArr = [a, b];
+    let a = /\(/g.test(str);
+    let b = /\)/g.test(str);
 
-    return /^1?[\((?=\d{3}\))|\s]?[\((?=\d{3}\))|\s]?\d{3}[(?<=\(\d{3}))\)]?[-|\s]?\d{3}[-|\s]?\d{4}$/gm.test(str);
+    if ((a === true && b === true) || (a === false && b === false)) {
+        return /^1?[\(|\s)]?[\(|\s]?\d{3}[\)]?[-|\s]?\d{3}[-|\s]?\d{4}$/gm.test(str);
+    } else {
+        return false;
+    }
 }
 
 console.log(telephoneCheck("555-555-5555"));
+
+
